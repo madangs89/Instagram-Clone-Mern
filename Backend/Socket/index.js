f609fa0b5f0c7379e46de093d7e828967060e62a
MADANA-GS <madangsnaik@gmail.com>
2025-08-29 01:57:24 +0530

    New Bug Fixed

--- Backend/Socket/index.js
+++ Backend/Socket/index.js
@@ -1,9 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { Server } from "socket.io";
 import { createClient } from "redis";
 import { createAdapter } from "@socket.io/redis-adapter";
-import dotevn from "dotenv";
-dotevn.config();
-const io = new Server(3005, {
+import cors from "cors";
+
+import express from "express";
+import http from "http";
+
+const app = express();
+const s = http.createServer(app);
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+
+const io = new Server(s, {
   cors: {
     origin: process.env.CLIENT_URL,
   },
@@ -13,13 +29,13 @@ const pubClient = createClient({
   url: process.env.REDIS_URL,
 });
 const subClient = pubClient.duplicate();
-
+pubClient.on("error", (err) => console.log("Redis Client Error", err));
+subClient.on("error", (err) => console.log("Redis Client Error", err));
 await pubClient.connect();
 await subClient.connect();
 
 io.adapter(createAdapter(pubClient, subClient));
 
-pubClient.on("error", (err) => console.log("Redis Client Error", err));
 const customPubClient = pubClient.duplicate();
 await customPubClient.connect();
 const customSubClient = pubClient.duplicate();
@@ -93,6 +109,7 @@ await customSubClient.subscribe("userComesOnline", async (data) => {
 
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
+  if (!userId) return socket.disconnect();
   console.log("a user connected", userId, socket.id);
   // Save user online state
   pubClient.hSet(
@@ -122,4 +139,12 @@ io.on("connection", (socket) => {
     console.log("a user disconnected", userId);
     await pubClient.hDel("onlineUsers", userId);
   });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+s.listen(process.env.PORT || 3005, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3005}`);
 });
